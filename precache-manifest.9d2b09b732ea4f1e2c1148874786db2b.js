8f757c410d7919ba30ec78628bd880ad44234333
Alex <alexandre.follin@gmail.com>
2019-12-21 04:17:36 +0100

    Updates

---
+++ precache-manifest.9d2b09b732ea4f1e2c1148874786db2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8bf044256e3d0c4c3eee2ee7497fe50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "618a46538d78eea471c8",
+    "url": "/static/css/main.86fa8334.chunk.css"
+  },
+  {
+    "revision": "e44ed2331c3aad7beefe",
+    "url": "/static/js/2.20ccc8fc.chunk.js"
+  },
+  {
+    "revision": "dae2a6f9fe909195a753b89a54ddd86b",
+    "url": "/static/js/2.20ccc8fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "618a46538d78eea471c8",
+    "url": "/static/js/main.c6e758e0.chunk.js"
+  },
+  {
+    "revision": "07b1c2952815dcdcc924",
+    "url": "/static/js/runtime-main.8359d5a8.js"
+  }
+]);
