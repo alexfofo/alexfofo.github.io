497a15c79fb8b3da7573dab30e1c28654cee6414
Alex <alexandre.follin@gmail.com>
2019-12-28 16:47:34 +0100

    add css on used letters

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import Popup from "reactjs-popup"
 
-const WORDS = ['poulpe', 'dauphin', 'lanterne', 'oreille', 'oiseau', 'livre', 'iphone', 'orange', 'entreprise', 'japon', 'universite']
+const WORDS = ['poulpe', 'dauphin', 'lanterne', 'oreille', 'oiseau', 'livre', 'iphone', 'orange', 'entreprise', 'japon', 'universite', 'casquette', 'lapin', 'merveille', 'manger', 'eau', 'vert', 'chemin', 'lynx', 'image', 'construire', 'personne', 'croire', 'emballer']
 const LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
   constructor (props) {
     super(props)
     const word = this.generateWTF()
-    this.state = { wordToFind: word, display: "_".repeat(word.length), countDown: 8 }
+    this.state = { wordToFind: word, display: "_".repeat(word.length), countDown: 8, indexUsed: [] }
   }
 
   getRandomInt(max) {
@@ -22,7 +22,7 @@ class App extends Component {
   }
 
   handleLetterClick(index) {
-    const { wordToFind, countDown } = this.state
+    const { wordToFind, countDown, indexUsed } = this.state
     var displayAsTab = this.state.display.split('')
     const letter = LETTERS[index]
     const regex = new RegExp(letter, 'gi')
@@ -34,16 +34,16 @@ class App extends Component {
     while ( (result = regex.exec(wordToFind)) ) {
         displayAsTab[result.index] = letter
     }
-    this.setState({ display: displayAsTab.join('') })
+    this.setState({ display: displayAsTab.join(''), indexUsed: [...indexUsed, index] })
   }
 
   handleRetry() {
     const word = this.generateWTF()
-    this.setState({ wordToFind: word, display: "_".repeat(word.length), countDown: 8 })
+    this.setState({ wordToFind: word, display: "_".repeat(word.length), countDown: 8, indexUsed: [] })
   }
 
   render() {
-    const { display, countDown } = this.state
+    const { display, countDown, indexUsed } = this.state
     const won = display.indexOf('_') === -1
     const loss = countDown <= 0
 
@@ -54,21 +54,23 @@ class App extends Component {
         <p className="displayWord">{ display }</p>
         <div className="alphabet">
         {LETTERS.split('').map((letter, index) => (
-            <span className="letters" onClick={() => {this.handleLetterClick(index)}} key={index}>{ letter }</span>
+            <span className={`letters ${indexUsed.includes(index) ? "indexUsed" : "" }`} onClick={() => {this.handleLetterClick(index)}} key={index}>
+              { letter }
+            </span>
           ))}
         </div>
         <br/>
         <Popup open={won} modal closeOnDocumentClick={false} >
           <div className="modal">
             <p>Bien joué t'es poulpement fortiche toi</p>
-            <button type="button" onClick={() => this.handleRetry()}>Retry  ?</button>
+            <button type="button" onClick={() => this.handleRetry()}>Encore ?</button>
           </div>
         </Popup>
 
         <Popup open={loss} modal closeOnDocumentClick={false} >
           <div className="modal">
-            <p>et merceeeeeeeee</p>
-            <button type="button" onClick={() => this.handleRetry()}>Non d'un encrier <br />je vais ré essayer</button>
+            <p>C'est raté</p>
+            <button type="button" onClick={() => this.handleRetry()}>Non d'un encrier <br />Je veux ré essayer</button>
           </div>
         </Popup>
 
